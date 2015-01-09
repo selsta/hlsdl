6881a22057851d5337c850f6f583af5fea5a4a68
selsta <selsta@home>
2015-01-09 11:11:37 +0100

    main: remove encryption msg
    don't like it.

--- main.c
+++ main.c
@@ -74,10 +74,6 @@ int main(int argc, const char * argv[]) {
     
     MSG_VERBOSE("Media Playlist parsed, downloading now!\n");
     
-    if (media_playlist.encryption) {
-        MSG_VERBOSE("Encryption found.\n");
-    }
-    
     if (download_hls(&media_playlist)) {
         return 1;
     }
