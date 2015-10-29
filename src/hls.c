a3f1b880e7055b3da4189afd4f39689d110092fb
selsta <selsta@sent.at>
2015-10-29 10:35:40 +0100

    hls: replace deprecated api usage
    copying mpvs commit...

--- src/hls.c
+++ src/hls.c
@@ -475,7 +475,7 @@ static int decrypt_sample_aes(struct hls_media_segment *s, struct ByteBuffer *bu
                 MSG_WARNING("Writing video frame failed.\n");
             }
         }
-        av_free_packet(&pkt);
+        av_packet_unref(&pkt);
     }
     if (av_write_trailer(ofmt_ctx) != 0) {
         MSG_ERROR("Writing trailer failed.\n");
